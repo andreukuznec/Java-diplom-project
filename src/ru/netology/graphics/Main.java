0d8cf221455d8499d6484d14929968001efd5bc2
andreukuznec <92484311+andreukuznec@users.noreply.github.com>
2023-10-30 21:35:04 +0100

    Update Main.java

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -9,12 +9,12 @@ import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        TextGraphicsConverter converter = new Converter(); // Создайте тут объект вашего класса конвертера
+        TextGraphicsConverter converter = new Converter(); 
 
-        GServer server = new GServer(converter); // Создаём объект сервера
-        server.start(); // Запускаем
+        GServer server = new GServer(converter); 
+        server.start(); 
 
-        // Или то же, но с выводом на экран:
+      
         String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
         String imgTxt = converter.convert(url);
         System.out.println(imgTxt);
