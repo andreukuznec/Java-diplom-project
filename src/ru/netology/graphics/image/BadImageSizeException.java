ec0d4cb82251d25bc7c7df026943ef9ce0c34ad8
andreukuznec <92484311+andreukuznec@users.noreply.github.com>
2023-10-30 21:36:07 +0100

    Update BadImageSizeException.java

--- src/ru/netology/graphics/image/BadImageSizeException.java
+++ src/ru/netology/graphics/image/BadImageSizeException.java
@@ -3,6 +3,6 @@ package ru.netology.graphics.image;
 
 public class BadImageSizeException extends Exception {
     public BadImageSizeException(double ratio, double maxRatio) {
-        super("Максимальное соотношение сторон изображения " + maxRatio + ", а у этой " + ratio);
+        super("Maximum image aspect ratio " + maxRatio + ", but this has " + ratio);
     }
 }
